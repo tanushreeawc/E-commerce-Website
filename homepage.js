984696a15da72ec57ad39d7429e0d0d942065f71
Tanushree Awchat <127086543+tanushreeawc@users.noreply.github.com>
2023-07-29 23:59:14 +0530

    Create homepage.js

---
+++ homepage.js
@@ -0,0 +1,8 @@
+// Get the paymentButton element by its id
+var btn = document.getElementById('btn');
+  
+// Add a click event listener to the paymentButton
+btn.addEventListener('click', function() {
+  // Load the payment.html file into the current page
+  window.location.href = 'index.html';
+});
